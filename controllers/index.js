9a2decb90fe0595f5bcc6965491f7d8279b69119
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-17 22:19:37 +0800

    update controller, add image kit uplaoad for qr code

--- controllers/index.js
+++ controllers/index.js
@@ -2,16 +2,38 @@ const Validator = require('fastest-validator');
 const qr = require('qr-image');
 const valid = new Validator;
 module.exports = {
-    generateQR: (req, res, next) => {
+    uploadImage: (file) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+                const uploadedFile = await imagekit.upload({
+                    file,
+                    fileName: 'qr-code'
+                });
+
+                const data = {
+                    title: uploadedFile.name,
+                    url: uploadedFile.url
+                };
+
+                resolve(data);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    },
+
+    generateQR: async(req, res, next) => {
         try {
             // create the qr
             const data = qr.imageSync('http://google.com', { type: 'png' });
             const qrString = data.toString('base64'); 
 
+            const qrImage = await uploadImage(qrString.toString('base64'));
+
             return res.status(200).json({
                 status: true,
                 message: "QR Generated",
-                data: qrString
+                data: qrImage
             });
         } catch(err) {
             next(err);
